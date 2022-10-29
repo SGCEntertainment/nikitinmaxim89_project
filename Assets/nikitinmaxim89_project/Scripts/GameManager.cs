1ba40d0601d1c45e0c9b4af7df2b3fa0c5974183
Sladkiy Eduard <smallgamescompany@gmail.com>
2022-10-29 20:09:00 +0300

    init

---
+++ Assets/nikitinmaxim89_project/Scripts/GameManager.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.Android;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    bool IsPhotoMaking;
+    const float timeOffset = 0.25f;
+
+    AndroidJavaClass jcUnityPlayer;
+    AndroidJavaObject joUnityActivity;
+    AndroidJavaObject joAndroidPluginAccess;
+    AndroidJavaObject context;
+
+    [SerializeField] Button makePhotoBtn;
+    public static Action OnPhotoStartMaking { get; set; } = delegate { };
+    public static Action OnPhotoFinishMaking { get; set; } = delegate { };
+
+    private void Start()
+    {
+        Screen.fullScreen = false;
+
+        jcUnityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        joUnityActivity = jcUnityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+        joAndroidPluginAccess = new AndroidJavaObject("com.flipmorris.flashlightcoremodule.Controller");
+        context = joUnityActivity.Call<AndroidJavaObject>("getApplicationContext");
+
+        makePhotoBtn.onClick.AddListener(() =>
+        {
+            if (!Permission.HasUserAuthorizedPermission(Permission.Camera))
+            {
+                Permission.RequestUserPermission(Permission.Camera);
+            }
+            else
+            {
+                if(IsPhotoMaking)
+                {
+                    return;
+                }
+
+                StartCoroutine(nameof(MakePhoto));
+            }
+        });
+    }
+
+    IEnumerator MakePhoto()
+    {
+        IsPhotoMaking = true;
+        OnPhotoStartMaking?.Invoke();
+        joAndroidPluginAccess.Call("EnableFlashlight", context, true);
+
+        yield return new WaitForSeconds(timeOffset);
+
+        joAndroidPluginAccess.Call("EnableFlashlight", context, false);
+        OnPhotoFinishMaking?.Invoke();
+        IsPhotoMaking = false;
+    }
+}
